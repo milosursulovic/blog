6e19889f0a3e426faf0b7d2dd5845557650d67b7
Milos Ursulovic <milosursulovic2696@gmail.com>
2022-01-25 13:06:16 +0100

    Enabled save article to db and changed dependencies in package.json

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const path = require('path')
 const mongoose = require('mongoose')
 
 //connecting to db
-mongoose.connect('mongodb://localhost/blog')
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 //connect to db once
 const db = mongoose.connection
@@ -16,32 +19,55 @@ db.on('error', error => console.log(error))
 //init app
 const app = express()
 
-//models
+//MODELS
 const Article = require('./models/article')
 
 //set view engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-//routes
+//MIDDLEWARES
 
-//get articles router, fetch from db
+//bodyparser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//ROUTES
+
+//render articles, fetching from db
 app.get('/', (req, res) => {
     Article.find({}, (error, articles) => {
-        if (error) console.log(error)
-        else {
-            res.render('index', {
-                title: 'Articles',
-                articles: articles
-            })
+        if (error) {
+            console.log(error)
+            return
         }
+        res.render('index', {
+            title: 'Articles',
+            articles: articles
+        })
     })
 })
 
-//add article route
+//render add article page
 app.get('/articles/add', (req, res) => {
     res.render('add_article', {
         title: 'Add Articles'
+    })
+})
+
+//save article to db
+app.post('/articles/add', (req, res) => {
+    const article = new Article()
+    article.title = req.body.title
+    article.author = req.body.author
+    article.body = req.body.body
+
+    article.save(error => {
+        if (error) {
+            console.log(error)
+            return
+        }
+        res.redirect('/')
     })
 })
 
